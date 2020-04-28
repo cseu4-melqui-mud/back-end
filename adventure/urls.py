8f2bc8a5114e0d89b9d40fef07c4b15bfd1c4ff3
Karim B <mattiakarim@live.it>
2020-04-28 10:59:44 +0200

    Add new url to the patterns in urls.py file

--- adventure/urls.py
+++ adventure/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import api
 
 urlpatterns = [
+    url('generateMap', api.generateMap),
     url('init', api.initialize),
     url('move', api.move),
     url('say', api.say),
