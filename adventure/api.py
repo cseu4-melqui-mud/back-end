fd37f7a5d18e658bce1732e80c64ec1afc5d6558
Karim B <mattiakarim@live.it>
2020-04-28 10:59:14 +0200

    Add initial pseudo code for possible ways to generate world

--- adventure/api.py
+++ adventure/api.py
@@ -11,6 +11,35 @@ import json
 # instantiate pusher
 # pusher = Pusher(app_id=config('PUSHER_APP_ID'), key=config('PUSHER_KEY'), secret=config('PUSHER_SECRET'), cluster=config('PUSHER_CLUSTER'))
 
+@api_view(["GET"])
+def generateWorld(request):
+    # maybe get from the user request the size of the maze, 100 or 500??
+
+    # clear previously generated world >> (i.e. using - Room.objects.delete())
+
+    for i in range(0, 100)
+        # choose a random type of room to generate within instantiation
+        # create new intance of the room, with all necessary properties
+
+        # add all properties, like a randomized title and description
+
+        # save room at each iteration on the matrix using >> .save()
+
+    # iterate over matrix once more and create connections between rooms using a corridor
+        # use both x,y axis and see if when room is reached if it has
+        # already a connection on that cardinal point or not
+            # if not connect
+            # else try changing direction
+                # if possible make connection
+                # else try opposite direction
+                # if not possible maybe connect also to previously connected room?
+    
+    # return JSON version serialized version of the world to the client
+    # research how to return serialized dictionaries, etc..
+
+# for i in range(0, 500):
+#     # generate map using comb of rooms ...
+
 @csrf_exempt
 @api_view(["GET"])
 def initialize(request):
